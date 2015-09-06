9f2c8e2037f5966c42247608a2b926d6c8773f21
Dany Cheong <danycheong@unitec.edu>
2015-09-06 11:10:38 -0600

    Binary Client

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ string GenerarId();
 string GenerarIdCiudad();
 string GenerarTelefono();
 void CityToBinary();
+void ClientToBinary();
 const int HeaderSize = sizeof(int) + sizeof(int) + sizeof(bool);
 
 int main(int argc, char* argv[]){
@@ -88,4 +89,49 @@ void CityToBinary(){
 	file.close();
 	outputFile.close();
 	//ListCity();
+}
+void ClientToBinary(){
+	ifstream file("Clientes.txt");
+	ofstream outputFile("Clientes.bin");
+	int rrn=-1, recordNumber = 0;
+	bool indexFlag = 0;
+	outputFile.write( reinterpret_cast<char*>(&rrn), sizeof(int) );
+	outputFile.write( reinterpret_cast<char*>(&recordNumber), sizeof(int) );
+	outputFile.write( reinterpret_cast<char*>(&indexFlag), sizeof(bool)  );
+	while(true){
+		if(file.eof())
+			break;
+		char IdClient[14];
+		char NameClient[40];
+		char Gender[2];
+		char IdCiudad[4];
+		string temp1 = "", temp2 = "",temp3 = "",temp4 = "";
+		getline(file, temp1, ',');
+		getline(file, temp2, ',');
+		getline(file, temp3, ',');
+		getline(file, temp4, ',');
+		for (int i = 0; i < 14; ++i){
+			IdClient[i] = temp1[i];
+		}		
+		for (int i = 0; i < 40; ++i){
+			NameClient[i] = temp2[i];
+		}
+		for (int i = 0; i < 2; ++i){
+			Gender[i] = temp3[i];
+		}		
+		for (int i = 0; i < 4; ++i){
+			IdCiudad[i] = temp4[i];
+		}
+		outputFile.write((char*)IdClient, sizeof(IdClient));
+		outputFile.write((char*)NameClient, sizeof(NameClient));
+		outputFile.write((char*)Gender, sizeof(Gender));
+		outputFile.write((char*)IdCiudad, sizeof(IdCiudad));
+		recordNumber++;
+  		outputFile.seekp (sizeof(rrn));
+  		outputFile.write(reinterpret_cast<char*>(&recordNumber), sizeof(recordNumber));
+  		outputFile.seekp(sizeof(int)*2 +1 +(recordNumber*60));
+	}	
+	file.close();
+	outputFile.close();
+	//ListClient();
 }
