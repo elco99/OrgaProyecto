06a6afb58ba66ff1d85d8c527ca57fd41e044078
Dany Cheong <danycheong@unitec.edu>
2015-09-06 10:41:43 -0600

    Generar

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include <cstring>
 
 using namepsace std;
+
+string GenerarId();
+string GenerarIdCiudad();
+string GenerarTelefono();
 
 int main(int argc, char* argv[]){
 
 	return 0;
+}
+
+string GenerarId(){
+	string id;
+	srand(time(0));
+	stringstream ss;
+	ss << rand() % 2 << rand() % 10<< rand() % 9<< rand() % 10 << (rand() % 98 + 1900) << (rand() % 90000+10000);
+	id  = ss.str();
+
+	return id;
+}
+string GenerarIdCiudad(){
+	string id;
+	srand(time(0));
+	stringstream ss;
+	ss << rand() % 10 << rand() % 10<< rand() % 10<< rand() % 10 ;
+	id  = ss.str();
+	return id;
+}
+string GenerarTelefono(){
+	string id;
+	srand(time(0));
+	stringstream ss;
+	ss << rand() % 10 << rand() % 10<< rand() % 10<< rand() % 10<< rand() % 10 << rand() % 10<< rand() % 10<< rand() % 10 ;
+	id  = ss.str();
+	return id;
 }
